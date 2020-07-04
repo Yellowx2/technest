76102559ab9dc9af34369961f0e78c953e77e76f
Víctor Huang Huang <vic-16@hotmail.com>
2020-07-04 16:48:02 +0200

    Base project with dependencies

---
+++ src/main/java/com/victor/technest/TechnestApplication.java
@@ -0,0 +1,13 @@
+package com.victor.technest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechnestApplication.class, args);
+	}
+
+}
